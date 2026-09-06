9027bb7d823bbbe3954aa30ba42ecee0c849aff0
kamillaarm <krms@cin.ufpe.br>
2026-09-06 03:10:55 -0300

    feat: add Animal class

---
+++ classes/animal/animal.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+
+#include "classes/animal.h"
+
+//Constructor
+// DataType's constructor initializes the inherited part of the Animal object.
+Animal::Animal(string currentname, string currenttype, string currentproduces, int currentdaysToAdult, int currentbuyPrice, int currentsellValue, vector<pair<string, string>> currentartisanItem): DataType(currentname, currenttype) {
+    produces = currentproduces;
+    daysToAdult = currentdaysToAdult;
+    buyPrice = currentbuyPrice;
+    sellValue = currentsellValue;
+    artisanItem = currentartisanItem;
+
+}
+
+// Destructor
+Animal::~Animal() {};
+
+// Getters
+string Animal::getProduces() const {
+    return produces;
+}
+int Animal::getDaysToAdult() const {
+    return daysToAdult;
+}
+int Animal::getBuyPrice() const {
+    return buyPrice;
+}
+int Animal::getSellValue() const {
+    return sellValue;
+}
+vector<pair<string, string>> Animal::getArtisanItem() const {
+    return artisanItem;
+}
+
+
+// Setters
+
+void Animal::setProduces(string newproduces) {
+    produces = newproduces;
+}
+void Animal::setDaysToAdult(int newdaysToAdult) {
+    daysToAdult = newdaysToAdult;
+}
+
+void Animal::setBuyPrice(int newbuyPrice) {
+    buyPrice = newbuyPrice;
+
+}
+
+void Animal::setSellValue(int newsellValue) {
+    sellValue = newsellValue;
+}
+
+void Animal::setArtisanItem(vector<pair<string, string>> newartisanItem) {
+    artisanItem = newartisanItem;
+}
+
+
---
+++ classes/animal/animal.h
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <utility>
+using namespace std;
+
+
+#ifndef ANIMAL
+#define ANIMAL
+
+#include "classes/datatype.h"
+
+
+class Animal : public DataType // Animal is a derived class from DataType and inherits its public members.
+
+{
+   private:
+   string produces;
+   int daysToAdult;
+   int buyPrice;
+   int sellValue;
+   vector<pair<string, string>> artisanItem;
+
+   public:
+
+   // Constructor
+   Animal(string currentname, string currenttype, string currentproduces, int currentdaysToAdult, int currentbuyPrice, int currentsellValue, vector<pair<string, string>> currentartisanItem);
+
+   // Destructor
+   virtual ~Animal(); 
+
+   // Getters
+   string getProduces() const;
+   int getDaysToAdult() const;
+   int getBuyPrice() const;
+   int getSellValue() const;
+   vector<pair<string, string>> getArtisanItem() const;
+
+
+   // Setters
+   void setProduces(string newproduces);
+   void setDaysToAdult(int newdaysToAdult);
+   void setBuyPrice(int newbuyPrice);
+   void setSellValue(int newsellValue);
+   void setArtisanItem(vector<pair<string, string>> newartisanItem);
+};
+
+#endif
