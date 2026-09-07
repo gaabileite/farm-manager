8cc82af6f1a49fe4e49c2ea6d079bad228d8b7a7
kamillaarm <krms@cin.ufpe.br>
2026-09-07 13:17:24 -0300

    refactor: move class implementations to main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <vector>
+#include <utility>
 #include "classes/datatype.h"
+#include "classes/animal.h"
+#include "classes/building.h"
 
 // Declaration of DataType's methods.
     // Constructor
@@ -27,3 +31,118 @@
     void DataType::setType(string newtype) {
         type = newtype;
     }
+
+// Declaration of Animal's methods.
+   // Constructor
+   // DataType's constructor initializes the inherited part of the Animal object.
+    Animal::Animal(string currentname, string currenttype, string currentproduces, int currentdaysToAdult, int currentbuyPrice, int currentsellValue, vector<pair<string, string>> currentartisanItem): DataType(currentname, currenttype) {
+        produces = currentproduces;
+        daysToAdult = currentdaysToAdult;
+        buyPrice = currentbuyPrice;
+        sellValue = currentsellValue;
+        artisanItem = currentartisanItem;
+    }
+
+    // Destructor
+    Animal::~Animal() {};
+
+    // Getters
+    string Animal::getProduces() const {
+        return produces;
+    }
+    int Animal::getDaysToAdult() const {
+        return daysToAdult;
+    }
+    int Animal::getBuyPrice() const {
+        return buyPrice;
+    }
+    int Animal::getSellValue() const {
+        return sellValue;
+    }
+    vector<pair<string, string>> Animal::getArtisanItem() const {
+        return artisanItem;
+    }
+
+    // Setters
+    void Animal::setProduces(string newproduces) {
+        produces = newproduces;
+    }
+    void Animal::setDaysToAdult(int newdaysToAdult) {
+        daysToAdult = newdaysToAdult;
+    }
+    void Animal::setBuyPrice(int newbuyPrice) {
+        buyPrice = newbuyPrice;
+    }
+    void Animal::setSellValue(int newsellValue) {
+        sellValue = newsellValue;
+    }
+    void Animal::setArtisanItem(vector<pair<string, string>> newartisanItem) {
+        artisanItem = newartisanItem;
+    }
+
+// Declaration of Building's methods.
+    // Constructor: no animals housed
+    // housesAnimals defaults to false.
+    Building::Building(string currentname, string currenttype, vector<pair<string, string>> currentconstructionMaterials, vector<int> currentsize, string currentwhereToGet): DataType(currentname, currenttype) {
+        constructionMaterials = currentconstructionMaterials;
+        size = currentsize;
+        whereToGet = currentwhereToGet;
+        housesAnimals = false;
+        // animalTypes stays as an empty vector
+        animalAmount = 0;
+    }
+
+    // Constructor: houses animals
+    Building::Building(string currentname, string currenttype, vector<pair<string, string>> currentconstructionMaterials, vector<int> currentsize, string currentwhereToGet, vector<string> currentanimalTypes, int currentanimalAmount): DataType(currentname, currenttype) {
+        constructionMaterials = currentconstructionMaterials;
+        size = currentsize;
+        whereToGet = currentwhereToGet;
+        housesAnimals = true;
+        animalTypes = currentanimalTypes;
+        animalAmount = currentanimalAmount;
+    }
+
+    // Destructor
+    Building::~Building() {};
+
+    // Getters
+    vector<pair<string, string>> Building::getConstructionMaterials() const {
+        return constructionMaterials;
+    }
+    vector<int> Building::getSize() const {
+        return size;
+    }
+    string Building::getWhereToGet() const {
+        return whereToGet;
+    }
+    bool Building::getHousesAnimals() const {
+        return housesAnimals;
+    }
+    vector<string> Building::getAnimalTypes() const {
+        return animalTypes;
+    }
+    int Building::getAnimalAmount() const {
+        return animalAmount;
+    }
+
+    // Setters
+    void Building::setConstructionMaterials(vector<pair<string, string>> newconstructionMaterials) {
+        constructionMaterials = newconstructionMaterials;
+    }
+    void Building::setSize(vector<int> newsize) {
+        size = newsize;
+    }
+    void Building::setWhereToGet(string newwhereToGet) {
+        whereToGet = newwhereToGet;
+    }
+    void Building::setHousesAnimals(bool newhousesAnimals) {
+        housesAnimals = newhousesAnimals;
+    }
+    void Building::setAnimalTypes(vector<string> newanimalTypes) {
+        animalTypes = newanimalTypes;
+    }
+    void Building::setAnimalAmount(int newanimalAmount) {
+        animalAmount = newanimalAmount;
+    }
+
+
